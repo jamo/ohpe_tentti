c2cb41b811578e8e18cfff3c97f13831f6a7b250
Jarmo Isotalo <jamo@isotalo.fi>
2013-02-14 01:44:16 +0200

    arvot oikein päin

--- db/migrate/20130213230846_add_depricated_to_answers.rb
+++ db/migrate/20130213230846_add_depricated_to_answers.rb
@@ -1,7 +1,7 @@
 class AddDepricatedToAnswers < ActiveRecord::Migration
   def up
     add_column :answers, :deprecated, :boolean ,:defaul => false
-    Answer.all.each {|a| a.update_attribute(:deprecated, false)}
+    Answer.all.each {|a| a.update_attribute(:deprecated, true)}
   end
 
   def down
