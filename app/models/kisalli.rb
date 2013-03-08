3328359f587bccc6a57bdba036e6b82ca3c9e237
Jarmo Isotalo <jamo@isotalo.fi>
2013-03-08 21:01:41 +0100

    validations...

--- app/models/kisalli.rb
+++ app/models/kisalli.rb
@@ -1,14 +1,14 @@
 class Kisalli < ActiveRecord::Base
   attr_accessible :aiemmin, :lempiolut, :miksi, :miksiSina, :mitaOdotat, :muuta, :name, :opintojen_ka, :opintopisteita, :opnro, :sposti, :ohjaaja, :kisalli, :tira, :ohpe, :ohja
 
-  validates :name, :uniqueness => true, :length => {:minimum => 2}
-  validates :sposti, :uniqueness => true, :length => {:minimum => 4}
-  validates :opnro, :uniqueness => true, :length => {:minimum => 7}
-  validates :aiemmin, :presence => true
-  validates :miksiSina, :presence => true
-  validates :mitaOdotat, :presence => true
-  validates :opintojen_ka, :presence => true
-  validates :opintopisteita, :presence => true
-
   scope :current, where(deprecated: false)
+
+  validates :name, :uniqueness => true, :length => {:minimum => 2}, on: current
+  validates :sposti, :uniqueness => true, :length => {:minimum => 4}, on: current
+  validates :opnro, :uniqueness => true, :length => {:minimum => 7}, on: current
+  validates :aiemmin, :presence => true, on: current
+  validates :miksiSina, :presence => true, on: current
+  validates :mitaOdotat, :presence => true, on: current
+  validates :opintojen_ka, :presence => true, on: current
+  validates :opintopisteita, :presence => true, on: current
 end
